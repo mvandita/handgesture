bf4ee1db8a4d3e6cc08fdfb56e835f14696c65e6
mvandita <86829958+mvandita@users.noreply.github.com>
2022-11-23 20:00:20 +0530

    Add files via upload

---
+++ count_fingers.py
@@ -0,0 +1,35 @@
+import cv2
+import mediapipe as mp
+cap = cv2.VideoCapture(0)
+mp_hands = mp.solutions.hands 
+mp_drawing = mp.solutions.drawing_utils 
+hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5)
+tipIds=[4,8,12,16,18]
+
+
+def drawHandLandMarks(image, hand_landmarks): 
+    if hand_landmarks:
+         for landmarks in hand_landmarks:
+             mp_drawing.draw_landmarks(image, landmarks, mp_hands.HAND_CONNECTIONS)
+
+
+while True:
+    success, image = cap.read()
+    image = cv2.flip(image, 1)
+    results = hands.process(image)
+    
+    hand_landmarks = results.multi_hand_landmarks
+    
+    drawHandLandmarks(image, hand_landmarks)
+
+
+    
+    
+    cv2.imshow("Media Controller", image)
+
+    key = cv2.waitKey(1)
+    if key == 32:
+        break
+
+cv2.destroyAllWindows()
+
